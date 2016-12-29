2fbf4e126f9339b5d224637e9fe59c19766c485e
Rafał Okuniewski <rafal.okuniewski@gmail.com>
2016-12-29 23:09:44 +0100

    Initial commit, working connection with remote DB

---
+++ MVCStudList/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MVCStudList.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStudList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Database.SetInitializer<StorageContext>(null);  // to wyłącza sprawdzanie migracji
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCStudList/Models/StorageContext.cs
@@ -0,0 +1,36 @@
+namespace MVCStudList.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    public partial class StorageContext : DbContext
+    {
+        public StorageContext()
+            : base("name=StorageContext")
+        {
+        }
+
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Group>()
+                .Property(e => e.Stamp)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Group>()
+                .HasMany(e => e.Students)
+                .WithRequired(e => e.Group)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Student>()
+                .Property(e => e.Stamp)
+                .IsFixedLength();
+        }
+    }
+}
